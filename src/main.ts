47c0adf902cb748fc9fb260188d56a405e0e0771
Eric Thayer <eric.thayer594@gmail.com>
2021-03-08 10:25:10 -0700

    typescript refactor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
+import VueApollo from "vue-apollo";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 createApp(App)
+  .use(VueApollo)
   .use(store)
   .use(router)
   .mount("#app");
